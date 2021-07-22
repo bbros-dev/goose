37b5f1e3782e5ee62dbdb5f8c03567f5600cb744
Begley Brothers Inc <begleybrothers@gmail.com>
2021-07-22 21:11:49 +1000

    [Add][CLI] Skeleton files
    Signed-off-by: Begley Brothers Inc <begleybrothers@gmail.com>

---
+++ regatta/bin/histogram.rs
@@ -0,0 +1,8 @@
+
+// Serialization:
+// [postcard](https://crates.io/crates/postcard): Postcard can serialize and deserialize messages similar to other serde formats. Using the default heapless feature to serialize to a heapless::Vec<u8>:
+// https://ferrous-systems.com/blog/test-embedded-app/#generating-random-inputs-with-quickcheck-testing-target2host
+//
+// https://github.com/bheisler/criterion.rs
+// https://github.com/sharkdp/hyperfine
+//
