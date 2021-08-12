50c004aee2f78d1266f5a2eb20e3e1f93072e879
Begley Brothers Inc <begleybrothers@gmail.com>
2021-08-12 19:07:10 +1000

    [Add][Test] Mini-Redis shutdown type
    Signed-off-by: Begley Brothers Inc <begleybrothers@gmail.com>

--- regatta/examples/05-pages-stream-4-ok-sig-2-c.rs
+++ regatta/examples/05-pages-stream-4-ok-sig-2-c.rs
@@ -10,8 +10,55 @@ lazy_static! {
     static ref START_TIME: Instant = Instant::now();
 }
 
-pub mod mymod {
+pub mod cli {
 
+    /// Listens for the CLI shutdown signal.
+    ///
+    /// Shutdown is signalled using a `broadcast::Receiver`. Only a single value is
+    /// ever sent. Once a value has been sent via the broadcast channel, the server
+    /// should shutdown.
+    ///
+    /// The `Shutdown` struct listens for the signal and tracks that the signal has
+    /// been received. Callers may query for whether the shutdown signal has been
+    /// received or not.
+    #[derive(Debug)]
+    pub(crate) struct Shutdown {
+        /// `true` if the shutdown signal has been received
+        shutdown: bool,
+
+        /// The receive half of the channel used to listen for shutdown.
+        notify: broadcast::Receiver<()>,
+    }
+
+    impl Shutdown {
+        /// Create a new `Shutdown` backed by the given `broadcast::Receiver`.
+        pub(crate) fn new(notify: broadcast::Receiver<()>) -> Shutdown {
+            Shutdown {
+                shutdown: false,
+                notify,
+            }
+        }
+
+        /// Returns `true` if the shutdown signal has been received.
+        pub(crate) fn is_shutdown(&self) -> bool {
+            self.shutdown
+        }
+
+        /// Receive the shutdown notice, waiting if necessary.
+        pub(crate) async fn recv(&mut self) {
+            // If the shutdown signal has already been received, then return
+            // immediately.
+            if self.shutdown {
+                return;
+            }
+
+            // Cannot receive a "lag error" as only one value is ever sent.
+            let _ = self.notify.recv().await;
+
+            // Remember that the signal has been received.
+            self.shutdown = true;
+        }
+    }
     // This is the simplest signal handling in tokio.
     // We don't pass anything between the thread running `main` and the thread
     // `tokio::spawn`ed to run this function.
@@ -56,7 +103,7 @@ fn main() {
 async fn real_main() -> i32 {
     let (shutdown_tx, mut shutdown_rx) = tokio::sync::oneshot::channel();
     let mut some_shutdown_tx = Some(shutdown_tx);
-    let signals_task = tokio::spawn(mymod::handle_signal( some_shutdown_tx));
+    let signals_task = tokio::spawn(cli::handle_signal( some_shutdown_tx));
 
     println!("First 10 pages:\n{:?}", get_n_pages(10).await);
 
@@ -76,7 +123,6 @@ async fn real_main() -> i32 {
         Err(_) => {
             println!("Shutdown sender (tx) is dropped without sending.");
         },
-        _ => unreachable!(),
     };
     // `drop(tx)` with or without sending, is achieved by setting the
     // `Option` to `None`
