a15fac18ad455227b14c8026d9742911718faf48
Mark Van de Vyver <mark@taqtiqa.com>
2021-09-28 19:52:12 +1000

    [Add][Test] Metal IO server
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

--- regatta/benches/repro.rs
+++ regatta/benches/repro.rs
@@ -53,16 +53,15 @@
 // harness = false
 
 /// Cargo.toml: end
-
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use pprof::criterion::{Output, PProfProfiler};
 
-use tracing::{self, debug, error, info, span, trace, warn, Instrument as _, Level, Span};
 use tracing::instrument;
+use tracing::{self, debug, error, info, span, trace, warn, Instrument as _, Level, Span};
 
+use async_trait::async_trait;
 use futures::StreamExt;
 use lazy_static::lazy_static;
-use async_trait::async_trait;
 use std::convert::TryInto;
 
 lazy_static! {
@@ -73,7 +72,10 @@ const URI: &str = "https://127.0.0.1";
 
 extern crate futures;
 extern crate hyper;
-use hyper::{body::HttpBody as _, body::to_bytes, client::HttpConnector, Body, Client as HyperClient, Method, Request};
+use hyper::{
+    body::to_bytes, body::HttpBody as _, client::HttpConnector, Body, Client as HyperClient,
+    Method, Request,
+};
 extern crate num_cpus;
 
 use std::io;
@@ -81,9 +83,26 @@ use std::net::SocketAddr;
 use std::time;
 
 use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
+use std::io::{Read, Write};
 use tokio::net::TcpStream;
 
 static HELLO: &[u8] = b"Hello World!";
+
+static RESPONSE: &str = "HTTP/1.1 200 OK
+Content-Type: text/html
+Connection: keep-alive
+Content-Length: 13
+
+Hello World!
+";
+
+fn is_double_crnl(window: &[u8]) -> bool {
+    window.len() >= 4
+        && (window[0] == '\r' as u8)
+        && (window[1] == '\n' as u8)
+        && (window[2] == '\r' as u8)
+        && (window[3] == '\n' as u8)
+}
 
 #[async_trait]
 pub trait HttpClient: Send + Sync + Clone + 'static {
@@ -119,6 +138,136 @@ impl Client {
 
 fn http_client() -> HyperClient<hyper::client::HttpConnector> {
     HyperClient::new()
+}
+
+enum Msg {
+    Echo(String),
+}
+
+// This is a very neat pattern for stopping a thread...
+// After starting the thread:
+//     drop(worker.take());
+//
+// https://matklad.github.io/2018/03/03/stopping-a-rust-worker.html
+fn spawn_server() -> std::sync::mpsc::Sender<Msg> {
+    let (tx, rx) = std::sync::mpsc::channel();
+    std::thread::spawn(move || {
+        while let Ok(msg) = rx.recv() {
+            match msg {
+                Msg::Echo(msg) => {
+                    init_mio_server();
+                },
+            }
+        }
+        println!("The server has stopped!");
+    });
+    tx
+}
+
+// This is a lean TCP server for responding with Hello World! to a request.
+// https://github.com/sergey-melnychuk/mio-tcp-server
+use mio::{Events, Poll, Interest, Token};
+fn init_mio_server() {
+    let address = "127.0.0.1:8888";
+    // let listener = mio::net::TcpListener::bind(&address.parse().unwrap()).unwrap();
+    let mut listener = reuse_mio_listener(&address.parse().unwrap()).expect("Could not bind to addr");
+    let mut poll = mio::Poll::new().unwrap();
+    poll.registry().register(
+        &mut listener,
+        mio::Token(0),
+        mio::Interest::READABLE
+    )
+    .unwrap();
+
+    let mut counter: usize = 0;
+    let mut sockets: std::collections::HashMap<mio::Token, mio::net::TcpStream> =
+        std::collections::HashMap::new();
+    let mut requests: std::collections::HashMap<mio::Token, Vec<u8>> =
+        std::collections::HashMap::new();
+    let mut buffer = [0 as u8; 1024];
+
+    let mut events = mio::Events::with_capacity(1024);
+    loop {
+        poll.poll(&mut events, None).unwrap();
+        for event in &events {
+            match event.token() {
+                mio::Token(0) => loop {
+                    match listener.accept() {
+                        Ok((mut socket, _)) => {
+                            counter += 1;
+                            let token = mio::Token(counter);
+
+                            poll.registry().register(
+                                &mut socket,
+                                token,
+                                mio::Interest::READABLE
+                            )
+                            .unwrap();
+
+                            sockets.insert(token, socket);
+                            requests.insert(token, Vec::with_capacity(8192));
+                        }
+                        Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
+                        Err(_) => break,
+                    }
+                },
+                token if event.is_readable() => {
+                    loop {
+                        let read = sockets.get_mut(&token).unwrap().read(&mut buffer);
+                        match read {
+                            Ok(0) => {
+                                sockets.remove(&token);
+                                break;
+                            }
+                            Ok(n) => {
+                                let req = requests.get_mut(&token).unwrap();
+                                for b in &buffer[0..n] {
+                                    req.push(*b);
+                                }
+                            }
+                            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
+                            Err(_) => break,
+                        }
+                    }
+
+                    let ready = requests
+                        .get(&token)
+                        .unwrap()
+                        .windows(4)
+                        .find(|window| is_double_crnl(*window))
+                        .is_some();
+
+                    if ready {
+                        //let mut socket = sockets.get_mut(&token).unwrap();
+                        poll.registry().reregister(
+                            sockets.get_mut(&token).unwrap(),
+                            token,
+                            mio::Interest::WRITABLE
+                        )
+                        .unwrap();
+                    }
+                }
+                token if event.is_writable() => {
+                    requests.get_mut(&token).unwrap().clear();
+                    sockets
+                        .get_mut(&token)
+                        .unwrap()
+                        .write_all(RESPONSE.as_bytes())
+                        .unwrap();
+
+                    // Re-use existing connection ("keep-alive") - switch back to reading
+                    //let &socket = sockets.get(&token).unwrap();
+                    poll.registry().reregister(
+                        sockets.get_mut(&token).unwrap(),
+                        token,
+                        mio::Interest::READABLE
+                    )
+                    .unwrap();
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
 }
 
 /// This is the main function, evolved from this gist:
@@ -242,6 +391,19 @@ async fn reuse_listener(
     Ok(builder.listen(1024).expect("TCP listener"))
 }
 
+fn reuse_mio_listener(
+    addr: &std::net::SocketAddr,
+) -> Result<mio::net::TcpListener, std::convert::Infallible> {
+    let builder = match *addr {
+        std::net::SocketAddr::V4(_) => mio::net::TcpSocket::new_v4().expect("TCP v4"),
+        std::net::SocketAddr::V6(_) => mio::net::TcpSocket::new_v6().expect("TCP v6"),
+    };
+    builder.set_reuseport(true).expect("Reusable port");
+    builder.set_reuseaddr(true).expect("Reusable address");
+    builder.bind(*addr).expect("TCP socket");
+    Ok(builder.listen(1024).expect("TCP listener"))
+}
+
 /// This function defines errors that are per-connection. Which basically
 /// means that if we get this error from `accept()` system call it means
 /// next connection might be ready to be accepted.
@@ -321,7 +483,26 @@ async fn run_stream_ct(
 #[instrument]
 async fn capacity(count: usize) {
     debug!("About to init server");
-    init_real_server().await;
+    // Spawn a server thread setup to use the channel pattern for stopping..
+    ///https://matklad.github.io/2018/03/03/stopping-a-rust-worker.html
+    let mut server = Some(spawn_server());
+    if let Some(ref server) = server {
+        // Any String will start the server...
+        server.send(Msg::Echo("Start".to_string())).unwrap();
+        println!("The server WAS spawned!");
+    } else {
+        println!("The server was NOT spawned!");
+    };
+
+    let mut server2 = Some(spawn_server());
+    if let Some(ref server2) = server2 {
+        // Any String will start the server...
+        server2.send(Msg::Echo("Start".to_string())).unwrap();
+        println!("The server2 WAS spawned!");
+    } else {
+        println!("The server2 was NOT spawned!");
+    };
+    //init_real_server().await;
     debug!("About to init client");
     //let session = Client::new().client;
     let session: surf::Client = surf::Config::new()
@@ -354,6 +535,10 @@ async fn capacity(count: usize) {
     })
     .await
     .unwrap();
+    // Stop the server thread using the channel pattern...
+    // https://matklad.github.io/2018/03/03/stopping-a-rust-worker.html
+    drop(server.take().expect("MIO server stopped"));
+    drop(server2.take().expect("MIO server2 stopped"));
     println!(
         "Throughput: {:.1} request/s",
         1000000.0 * count as f64 / benchmark_start.elapsed().as_micros() as f64
